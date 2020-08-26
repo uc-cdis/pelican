b3f69d0cd37491d8dc92d5a2beb1830a69adb892
Mingfei Shao <mshao1@uchicago.edu>
2020-08-26 10:06:16 -0500

    fix: use guppy mapping

--- pelican/graphql/guppy_gql.py
+++ pelican/graphql/guppy_gql.py
@@ -21,22 +21,39 @@ class GuppyGQL(BaseGQL):
             count = 0
         return count
 
-    def _download_endpoint(self, filters=None):
+    def _mapping(self):
+        self.url = f"{self.hostname}/guppy/graphql"
+        query = f"query {{ _mapping {{ {self.node} }} }}"
+        query_json = {"query": query}
+
+        r = BaseGQL._execute(self, query_json)
+        try:
+            mapping_list = r["data"]["_mapping"][self.node]
+            node_name = [s for s in mapping_list if f"{self.node}_id" in s][0]
+        except IndexError:
+            node_name = None
+        return node_name
+
+    def _download_endpoint(self, filters=None, node_name=None):
         print("fallback to /download endpoint")
+        if node_name is None:
+            node_name = f"_{self.node}_id"
         self.url = f"{self.hostname}/guppy/download"
         query = {
             "type": self.node,
-            "fields": [f"_{self.node}_id"],
-            "accessibility": "accessible"
+            "fields": [node_name],
+            "accessibility": "accessible",
         }
         if filters:
             query.update(json.loads(filters))
         r = BaseGQL._execute(self, query)
         return r
 
-    def _graphql_endpoint(self, filters=None):
+    def _graphql_endpoint(self, filters=None, node_name=None):
+        if node_name is None:
+            node_name = f"_{self.node}_id"
         self.url = f"{self.hostname}/guppy/graphql"
-        query = f"query($filter: JSON) {{ {self.node}(first: 10000, filter: $filter, accessibility: accessible) {{ _{self.node}_id }} }}"
+        query = f"query($filter: JSON) {{ {self.node}(first: 10000, filter: $filter, accessibility: accessible) {{ {node_name} }} }}"
         query_json = {"query": query}
         if filters:
             query_json["variables"] = filters
@@ -49,13 +66,18 @@ class GuppyGQL(BaseGQL):
 
     def execute(self, filters=None):
         count = self._count(filters)
+        node_name = self._mapping()
         # Elasticsearch has a default limitation of 10000 for search results
         # https://www.elastic.co/guide/en/elasticsearch/reference/master/search-request-body.html#request-body-search-from-size
         # therefore, Pelican will use Guppy 'download' endpoint
         guppy_max_size = 10000
-        r = self._download_endpoint(filters) if count > guppy_max_size else self._graphql_endpoint(filters)
+        r = (
+            self._download_endpoint(filters, node_name)
+            if count > guppy_max_size
+            else self._graphql_endpoint(filters, node_name)
+        )
         try:
-            ids = [item[f"_{self.node}_id"] for item in r]
+            ids = [item[f"{node_name}"] for item in r]
         except KeyError:
             ids = []
         return ids
