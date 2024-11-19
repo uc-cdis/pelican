5d03aa446df31bf26e485c998d909db98947e7ae
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-11-19 15:34:58 -0700

    updating gdcdatamodel to gen3datamodel

--- pelican/dictionary.py
+++ pelican/dictionary.py
@@ -7,9 +7,9 @@ from dictionaryutils import DataDictionary, dictionary
 def init_dictionary(url):
     d = DataDictionary(url=url)
     dictionary.init(d)
-    # the gdcdatamodel expects dictionary initiated on load, so this can't be
+    # the gen3datamodel expects dictionary initiated on load, so this can't be
     # imported on module level
-    from gdcdatamodel import models as md
+    from gen3datamodel import models as md
 
     return d, md
 
