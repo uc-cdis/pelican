4f23671db37f9f374d4852ab002009ed2ff0df00
Spencer Axelrod <spencer.axelrod@gmail.com>
2025-04-25 03:21:45 -0500

    add changes from fix/import-db

--- job_import.py
+++ job_import.py
@@ -12,7 +12,8 @@ from pelican.dictionary import init_dictionary, DataDictionaryTraversal
 from pelican.s3 import download_file
 
 from sqlalchemy.sql import text
-from pelican.config import logger
+
+from gdcdatamodel.models.submission import Base
 
 if __name__ == "__main__":
     access_token = os.environ["ACCESS_TOKEN"]
@@ -23,8 +24,16 @@ if __name__ == "__main__":
 
     dictionary_url = os.environ["DICTIONARY_URL"]
 
-    with open("/sheepdog-creds.json") as pelican_creds_file:
-        sheepdog_creds = json.load(pelican_creds_file)
+    with open("/sheepdog-creds.json") as sheepdog_creds_file:
+        sheepdog_creds = json.load(sheepdog_creds_file)
+
+    with open("/admin-servers.json") as dbfarm_file:
+        servers = json.load(dbfarm_file)
+
+    # we look through the servers in the dbfarm to find the sheepdog db server
+    for s in servers:
+        if servers[s]["db_host"] == sheepdog_creds["db_host"]:
+            db_server = servers[s]
 
     if "guid" in input_data_json:
         logger.info("a guid was supplied to the job")
@@ -47,15 +56,18 @@ if __name__ == "__main__":
     # DB_URL = "jdbc:postgresql://{}/{}".format(
     #     sheepdog_creds["db_host"], sheepdog_creds["db_database"]
     # )
-    DB_USER = sheepdog_creds["db_username"]
-    DB_PASS = sheepdog_creds["db_password"]
+
+    # setup DB engine for postgres user
+    # this is needed as only the postgres user can create a new database and the sheepdog user cannot
+    DB_USER = db_server["db_username"]
+    DB_PASS = db_server["db_password"]
 
     NEW_DB_NAME = input_data_json["db"]
 
     # create a database in the name that was passed through
     engine = sqlalchemy.create_engine(
-        "postgres://{user}:{password}@{host}/postgres".format(
-            user=DB_USER, password=DB_PASS, host=sheepdog_creds["db_host"]
+        "postgresql://{user}:{password}@{host}/postgres".format(
+            user=DB_USER, password=DB_PASS, host=db_server["db_host"]
         )
     )
     conn = engine.connect()
@@ -63,20 +75,43 @@ if __name__ == "__main__":
 
     logger.info("we are creating a new database named ", NEW_DB_NAME)
 
-    create_db_command = text("create database :db")
+    create_str = "CREATE DATABASE {db}"
+    create_db_command = create_str.format(db=NEW_DB_NAME)
     logger.info("This is the db create command: ", create_db_command)
 
-    grant_db_access = text("grant all on database :db to sheepdog with grant option")
+    grant_str = "GRANT ALL ON DATABASE {db} TO {username} WITH GRANT OPTION"
+    grant_db_access = grant_str.format(
+        db=NEW_DB_NAME, username=sheepdog_creds["db_username"]
+    )
     logger.info("This is the db access command: ", grant_db_access)
-
     try:
-        conn.execute(create_db_command, db=NEW_DB_NAME)
-        conn.execute(grant_db_access, db=NEW_DB_NAME)
-    except Exception:
-        logger.error("Unable to create database")
+        conn.execute(create_db_command)
+        conn.execute("commit")
+
+        conn.execute(grant_db_access)
+        conn.execute("commit")
+    except Exception as e:
+        logger.error("Unable to create database... error: ", e)
         raise Exception
 
+    # close db connection for root user
     conn.close()
+
+    # setup the transaction tables for the db
+    logger.info("Setting up transaction tables for new sheepdog db")
+    PORT = "5432"
+    engine = sqlalchemy.create_engine(
+        "postgresql://{user}:{password}@{host}:{port}/{database}".format(
+            user=DB_USER,
+            host=db_server["db_host"],
+            port=PORT,
+            password=DB_PASS,
+            database=NEW_DB_NAME,
+        )
+    )
+
+    # create db transaction tables
+    Base.metadata.create_all(engine)
 
     DB_URL = "jdbc:postgresql://{}/{}".format(sheepdog_creds["db_host"], NEW_DB_NAME)
 
